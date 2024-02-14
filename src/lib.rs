8451075de0919b00df683275128135ee0703e699
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-02-14 16:55:54 -0500

    chore(cargo): Initialize project

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#[must_use]
+pub const fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[fixture]
+    fn setup() {
+        #[allow(clippy::unwrap_used)]
+        color_eyre::install().unwrap();
+    }
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
